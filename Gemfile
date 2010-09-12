29feb88879973100454b979bc681f735e6e6ce67
vlad <shegnaga@gmail.com>
2010-09-12 11:10:35 -0700

    User model with passwords, and user show page

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.0'
+gem 'gravatar_image_tag', '0.1.0'
 
 group :development do
   gem 'rspec-rails', '2.0.0.beta.18'
@@ -10,6 +11,7 @@ end
 group :test do
   gem 'rspec', '2.0.0.beta.18'
 	gem 'spork', '0.8.4'
+	gem 'factory_girl_rails', '1.0'
 end
 
 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,6 +4,11 @@ describe UsersController do
   render_views
 
   describe "GET 'new'" do
+    
+    before(:each) do
+      @user = Factory(:user)
+    end
+    
     it "should be successful" do
       get 'new'
       response.should be_success
@@ -12,6 +17,20 @@ describe UsersController do
     it "should have the right title" do
       get 'new'
       response.should have_selector("title", :content => "Sign up")
+    end
+    it "should include the user's name" do
+      get :show, :id => @user
+      response.should have_selector("h1", :content => @user.name)
+    end
+
+    it "should have a profile image" do
+      get :show, :id => @user
+      response.should have_selector("h1>img", :class => "gravatar")
+    end
+    
+    it "should find the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
     end
   end
 end
